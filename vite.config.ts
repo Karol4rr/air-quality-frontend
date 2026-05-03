b5b91b794a64e29dd22e1a9b1be206d91261edc5
Karol4rr <62398537+Karol4rr@users.noreply.github.com>
2026-05-03 20:18:19 +0200

    (fix-5)GithubPagesRoutingFix (#8)
    * (fix-5) Github Pages Routing Fix
    
    * (fix-5) Github Actions Plugin for 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
+import { copyFileSync } from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import { defineConfig } from 'vite';
+import { defineConfig, type Plugin } from 'vite';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+/** GitHub Pages workaround */
+const spaGithubPages404 = (): Plugin => {
+	return {
+		name: 'spa-github-pages-404',
+		closeBundle() {
+			const dist = path.resolve(__dirname, 'dist');
+			copyFileSync(path.join(dist, 'index.html'), path.join(dist, '404.html'));
+		},
+	};
+};
+
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss(), svgr()],
+export default defineConfig(({ command }) => ({
+	plugins: [react(), tailwindcss(), svgr(), ...(command === 'build' ? [spaGithubPages404()] : [])],
+	base: command === 'build' ? '/air-quality-frontend/' : '/',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
 		},
 	},
-});
+}));
